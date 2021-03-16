aaeb82b8ceb1d49ba34245092448c100c0564060
Gjinaj Gledis <gledis.gjnaj@sisal.it>
2021-03-16 10:57:38 +0100

    Add main

---
+++ src/main/java/al/gjinaj/Main.java
@@ -0,0 +1,7 @@
+package al.gjinaj;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/al/gjinaj/MainTest.java
@@ -0,0 +1,11 @@
+package al.gjinaj;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test(){
+      System.out.println("Hello tests!");
+    }
+}
